63e9976e4ffd097e7a10ec3be443901b923d26fa
Sarah Ivey <si102623@ohio.edu>
2026-03-17 12:14:46 -0400

    reorganizing the files in order to make flask routes

--- src/main.py
+++ src/main.py
@@ -1,14 +1,16 @@
 from flask import Flask, render_template
+from templates.stripselect.stripselect import stripselect_app
 
 app = Flask(__name__)
+app.register_blueprint(stripselect_app, url_prefix='/stripselect')
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/next")
-def next_page():
-    return render_template("next.html")
+@app.route("/stripselect", methods=["GET", "POST"])
+def stripselect():
+    return render_template("stripselect/stripselect.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
